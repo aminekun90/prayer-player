1ab8eded46b0e748c33c4ad3486d8a076ad2e114
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2024-02-01 01:12:18 +0100

    update Docker file

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import logging.handlers
 from flask_app import FlaskApp
@@ -8,5 +9,12 @@ logging.basicConfig(handlers=[logging.FileHandler('logs/app.log', 'w+', 'utf-8')
                               ], format=FORMAT, level=logging.INFO, datefmt='[%Y-%m-%dT%H:%M:%SZ%z')
 
 if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(
+    #     description="Prometheus exporter for Sonos networks")
+    # parser.add_argument("--address", default="localhost:1915",
+    #                     help="Network address for this server")
+    # args = parser.parse_args()
+    # host, port = args.address.split(":", 1)
+
     flask_app = FlaskApp()
     flask_app.run()
