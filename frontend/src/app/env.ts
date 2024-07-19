640556018b7d7028fe0259ed34212466b8047088
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2024-07-19 11:00:04 +0200

    Version 1.1.0

--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,3 +1,3 @@
 import packageInfo from "../../package.json";
 export const API_URL = 'http://localhost:8000';
-export const VERSION = 'Beta Build' + packageInfo.version;
+export const VERSION = 'Beta Build ' + packageInfo.version;
